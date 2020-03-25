f7efcc3f7b2b5827199511a54f75590b79f498fe
Chris Pressey <cpressey@catseye.tc>
2020-03-25 16:59:36 +0000

    Add more example programs.

--- eg/examplePrograms.jsonp.js
+++ eg/examplePrograms.jsonp.js
@@ -1,6 +1,22 @@
 examplePrograms = [
     {
+        "contents": "#1#1+\n",
+        "filename": "add-one-and-one.emmental"
+    },
+    {
+        "contents": ";#58#126#63#36!;#46#36#!;#0#1!;#0#2!;#0#3!;#0#4!;#0#5!;#0#6!;#0#7!\n#0#33#111#108#108#101#72$\n",
+        "filename": "hello.emmental"
+    },
+    {
         "contents": "#59#35#55#56#46#!;##1!;##2!;##3!;##4!;##5!;##6!;##7!#59#35#56#57#46#8!,#77-~?\n",
-        "filename": "testprog10.emmental"
+        "filename": "is-the-input-M.emmental"
+    },
+    {
+        "contents": ";#43#38!#1#1&\n",
+        "filename": "redefine-ampersand-as-add.emmental"
+    },
+    {
+        "contents": "#67#66#65^v^-+^^v^v^v-+^v-+^v-+vv\n",
+        "filename": "swap-top-of-stack.emmental"
     }
 ];
